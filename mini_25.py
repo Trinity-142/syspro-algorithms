935cce51903abeb2336bccf82264c2e1201797d2
Trinity-142 <n.sharapov.nsk@gmail.com>
2025-04-21 19:12:27 +0700

    #25

---
+++ mini_25.py
@@ -0,0 +1,62 @@
+import math
+import random
+from typing import Tuple
+
+from pympler import asizeof
+
+
+def next_prime(x):
+    while not is_prime(x):
+        x += 1
+    return x
+
+
+def is_prime(x):
+    if x < 2: return False
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+class BloomFilter:
+    def __init__(self, s, e):
+        self.bitset = 0
+        self.s = s
+        self.e = e
+        self.b = math.ceil(math.log(self.e, 1 / 2) / math.log(2))
+        self.k = math.ceil(self.b * math.log(2))
+        self.n = next_prime(self.b * self.s)
+        self.weights = [tuple(random.randint(0, self.n - 1) for _ in range(4)) for _ in range(self.k)]
+
+    def _hash(self, i, ip: Tuple):
+        return sum(a * x for a, x in zip(self.weights[i], ip)) % self.n
+
+    def insert(self, ip: Tuple):
+        for i in range(self.k):
+            hash = self._hash(i, ip)
+            self.bitset |= (1 << hash)
+
+    def lookup(self, ip: Tuple) -> bool:
+        for i in range(self.k):
+            if not self.bitset & (1 << self._hash(i, ip)):
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    for s, e in [(100, 0.001), (500, 0.003), (1000, 0.008), (100000, 0.012)]:
+        fp = 0
+        f = BloomFilter(s, e)
+        ips = [tuple(random.randint(0, 255) for _ in range(4)) for i in range(s)]
+        other_ips = [tuple(random.randint(0, 255) for _ in range(4)) for i in range(s)]
+        for ip in ips:
+            f.insert(ip)
+        for other_ip in other_ips:
+            if f.lookup(other_ip) and other_ip not in ips:
+                fp += 1
+
+        memory = asizeof.asizeof(f)
+        print(f"Percent of FP: {fp / s}%\n"
+              f"Memory used: {memory / 1024} KB\n"
+              f"---------------------------------")
